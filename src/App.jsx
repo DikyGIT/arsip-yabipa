ddbec1de5607cb72f4a306b0752a4e82ffdf226a
DikyGIT <dikyoga27@gmail.com>
2023-09-20 18:04:40 +0700

    revisi style arsip

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import ArsipComponent from "./components/ArsipComponent";
 
 function App() {
   return (
-    <>
-      <div className="py-14">
+    <div className="py-14">
+      <div className="pb-14">
         <h1 className="font-bold text-center md:text-5xl text-4xl">ARSIP YABIPA</h1>
       </div>
 
       {/* Arsip Yabipa */}
       <ArsipComponent />
-    </>
+    </div>
   );
 }
 
